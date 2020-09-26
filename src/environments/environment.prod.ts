e9bee95261894454dd148b5e04f84e40fb4a5888
gaudymejia <gaudy.mejia1008@gmail.com>
2020-09-26 11:51:44 -0600

    Consume api for all services in the app

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,5 @@
 export const environment = {
-  production: true
+  name: 'TEST/DEBUG-SECURITY',
+  production: false,
+  URL_SecurityPaymentControl_RestApi: 'http://securitypaymentcontrolservices-prod.us-east-1.elasticbeanstalk.com/',
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
   name: 'TEST/DEBUG-SECURITY',
   production: false,
-  URL_SecurityPaymentControl_RestApi: 'http://localhost:57255/',
+  URL_SecurityPaymentControl_RestApi: 'http://securitypaymentcontrolservices-prod.us-east-1.elasticbeanstalk.com/',
 };
